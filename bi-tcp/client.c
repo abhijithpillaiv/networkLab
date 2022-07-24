f57a3f2c8c4c259f4d33bfdc36b12a9d7390fcd8
abhijithpillaiv <avp9795@gmail.com>
2022-07-24 23:42:23 +0530

    bi-tcp

---
+++ bi-tcp/client.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<string.h>
+
+void main(){
+    int skt;
+    char msg[10];
+    struct sockaddr_in clientadd,serveraddr;
+
+    // socket creation
+    skt=socket(AF_INET,SOCK_STREAM,0);
+    serveraddr.sin_family=AF_INET;
+    serveraddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serveraddr.sin_port=htons(33345);
+
+if (skt)
+{
+  // Connection establishment
+    connect(skt,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+    
+    // Input message and clr garbages
+    printf("\ntype :: ");
+    fgets(msg,20,stdin);
+    int len = strlen(msg);
+    msg[len-1]='\0';
+    
+    // send and receive
+    send(skt,msg,sizeof(msg),0);
+    recv(skt,msg,sizeof(msg),0);
+
+    printf("\n--> %s",msg);
+}
+
+  else{
+    printf("socket creation error");
+  }
+}
---
+++ bi-tcp/server.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+
+void main()
+{
+    int skt, nsd;
+    char msg[10];
+    struct sockaddr_in clientadd, serveraddr;
+
+    // socket creation
+    skt = socket(AF_INET, SOCK_STREAM, 0);
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(33345);
+
+    if (skt)
+    {
+        // Connection establishment in server bind
+        bind(skt, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+        listen(skt, 5);
+        int c_len = sizeof(clientadd);
+        nsd = accept(skt, (struct sockaddr *)&clientadd, &c_len);
+
+        // send and receive
+        recv(nsd, msg, sizeof(msg), 0);
+        printf("\n--> %s", msg);
+
+        // Input message and clr garbages
+        printf("\ntype :: ");
+        fgets(msg, 20, stdin);
+        int len = strlen(msg);
+        msg[len - 1] = '\0';
+
+        send(nsd, msg, sizeof(msg), 0);
+    }
+    else
+    {
+        printf("socket not created");
+    }
+}
